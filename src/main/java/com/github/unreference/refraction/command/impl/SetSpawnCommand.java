b799ac61b39ff702e33aa2f7fef8b03bfbbc4788
unreference <unreference@duck.com>
2024-09-30 08:03:48 -0700

    Simplify set spawn command

--- src/main/java/com/github/unreference/refraction/command/impl/SetSpawnCommand.java
+++ src/main/java/com/github/unreference/refraction/command/impl/SetSpawnCommand.java
@@ -1,6 +1,5 @@
 package com.github.unreference.refraction.command.impl;
 
-import com.github.unreference.refraction.Refraction;
 import com.github.unreference.refraction.command.AbstractCommand;
 import com.github.unreference.refraction.model.Rank;
 import com.github.unreference.refraction.util.MessageUtil;
@@ -10,9 +9,7 @@ import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SetSpawnCommand extends AbstractCommand {
 
@@ -22,7 +19,7 @@ public class SetSpawnCommand extends AbstractCommand {
 
     @Override
     public void trigger(CommandSender sender, String[] args) {
-        if (args.length > 3 && args.length != 4) {
+        if (args.length != 0 && args.length != 3) {
             sender.sendMessage(getUsageMessage());
             return;
         }
@@ -32,86 +29,44 @@ public class SetSpawnCommand extends AbstractCommand {
         World world = playerLocation.getWorld();
 
         if (args.length == 0) {
-            world.setSpawnLocation(playerLocation);
-            sender.sendMessage(MessageUtil.getPrefixedMessage(
-                    getPrefix(),
-                    "Set &b%s's &7spawn to &bXYZ&7: &b%.0f &7/ &b%.0f &7/ &b%.0f&7.",
-                    world.getName(),
-                    world.getSpawnLocation().getX(),
-                    world.getSpawnLocation().getY(),
-                    world.getSpawnLocation().getZ()));
-            return;
-        } else if (args.length == 3) {
+            setSpawn(player, playerLocation);
+        } else {
             try {
                 int x = Integer.parseInt(args[0]);
                 int y = Integer.parseInt(args[1]);
                 int z = Integer.parseInt(args[2]);
-
-                Location location = new Location(world, x, y, z);
-                world.setSpawnLocation(location);
-                sender.sendMessage(MessageUtil.getPrefixedMessage(
-                        getPrefix(),
-                        "Set &b%s's &7spawn to &bXYZ&7: &b%.0f &7/ &b%.0f &7/ &b%.0f&7.",
-                        world.getName(),
-                        world.getSpawnLocation().getX(),
-                        world.getSpawnLocation().getY(),
-                        world.getSpawnLocation().getZ()));
-                return;
+                setSpawn(player, new Location(world, x, y, z));
             } catch (NumberFormatException exception) {
-                sender.sendMessage(MessageUtil.getPrefixedMessage(getPrefix(), "Invalid coordinates."));
-                return;
+                sender.sendMessage(getUsageMessage());
             }
-        }
-
-        String worldInput = args[0];
-        world = Refraction.getPlugin().getServer().getWorld(worldInput);
-        if (world == null) {
-            sender.sendMessage(MessageUtil.getPrefixedMessage(getPrefix(), "World not found: &b%s", worldInput));
-            return;
-        }
-
-        try {
-            int x = Integer.parseInt(args[1]);
-            int y = Integer.parseInt(args[2]);
-            int z = Integer.parseInt(args[3]);
-
-            Location location = new Location(world, x, y, z);
-            world.setSpawnLocation(location);
-            sender.sendMessage(MessageUtil.getPrefixedMessage(
-                    getPrefix(),
-                    "Set &b%s's &7spawn to &bXYZ&7: &b%.0f &7/ &b%.0f &7/ &b%.0f&7.",
-                    world.getName(),
-                    world.getSpawnLocation().getX(),
-                    world.getSpawnLocation().getY(),
-                    world.getSpawnLocation().getZ()));
-        } catch (NumberFormatException exception) {
-            sender.sendMessage(MessageUtil.getPrefixedMessage(getPrefix(), "Invalid coordinates."));
         }
     }
 
     @Override
     public List<String> tab(CommandSender sender, String alias, String[] args) {
-        List<String> suggestions = new ArrayList<>();
-
-        if (args.length == 1) {
-            suggestions = Refraction.getPlugin().getServer().getWorlds().stream()
-                    .map(World::getName)
-                    .collect(Collectors.toCollection(ArrayList::new));
-            String currentArg = args[0];
-            filterTab(suggestions, currentArg);
-        }
-
-        return suggestions;
+        return List.of();
     }
 
     @Override
     protected Component getUsageMessage() {
         return MessageUtil.getPrefixedMessage(
-                getPrefix(), "Usage: /%s [coordinates|world] <coordinates>", getAliasUsed());
+                getPrefix(), "Usage: /%s [<coordinates>]", getAliasUsed());
     }
 
     @Override
     protected void generatePermissions() {
         Rank.ADMIN.grantPermission(getPermission(), true);
+    }
+
+    private void setSpawn(CommandSender sender, Location location) {
+        World world = location.getWorld();
+        world.setSpawnLocation(location);
+        sender.sendMessage(MessageUtil.getPrefixedMessage(
+                getPrefix(),
+                "Set &b%s's &7spawn to &b%.0f&7, &b%.0f&7, &b%.0f&7.",
+                world.getName(),
+                world.getSpawnLocation().getX(),
+                world.getSpawnLocation().getY(),
+                world.getSpawnLocation().getZ()));
     }
 }
